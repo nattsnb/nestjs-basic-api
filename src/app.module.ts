dcefb3b59b8ff2c5f9b9d998ea64978e07bd41c7
Natasza ASzatan <natiye.nb@gmai.com>
2025-03-01 23:39:52 +0100

    created Logger Classand imported its Module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ArticlesModule} from "./articles/articles.module";
+import {LoggerModule} from "./logger/logger.module";
 
 @Module({
-  imports: [ArticlesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ArticlesModule, LoggerModule],
 })
 export class AppModule {}
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,20 +1,27 @@
-import {Injectable, NotFoundException} from "@nestjs/common";
+import {Injectable, Logger, NotFoundException} from "@nestjs/common";
 import {Article} from "./article";
 import {ArticleDto} from "./article.dto";
+import {LoggerService} from "../logger/logger.service";
 
 @Injectable()
 export class ArticlesService {
+  constructor(private readonly loggerService: LoggerService) {}
+
   private articles: Article[] = [];
   private nextCreatedArticleId = 1;
 
   getAll() {
+    this.loggerService.log("Getting a list of all articles.")
     return this.articles;
   }
 
   getById(id: number) {
+    this.loggerService.log(`Getting article with id ${id}.`)
+
     const article = this.articles.find(article => article.id === id);
 
     if(!article) {
+      this.loggerService.warn("Trying to access article that doesn't exist.")
       throw new NotFoundException();
     }
 
@@ -22,6 +29,7 @@ export class ArticlesService {
   }
 
   create(article: ArticleDto) {
+    this.loggerService.log(`Creating new article.`)
     if(article) {
       const newArticle = {
         id: this.nextCreatedArticleId++,
@@ -33,11 +41,13 @@ export class ArticlesService {
   }
 
   update(id: number, article: ArticleDto) {
+    this.loggerService.log(`Updating article with id ${id}.`)
     const articleIndex = this.articles.findIndex(
       (article) => article.id === id,
     );
 
     if(articleIndex === -1) {
+      this.loggerService.warn("Trying to update article that doesn't exist.")
       throw new NotFoundException();
     }
 
@@ -51,11 +61,13 @@ export class ArticlesService {
   }
 
   delete(id: number) {
+    this.loggerService.log(`Deleting article with id ${id}.`)
     const articleIndex = this.articles.findIndex(
       (article) => article.id === id,
     );
 
     if(articleIndex === -1) {
+      this.loggerService.warn("Trying to delete article that doesn't exist.")
       throw new NotFoundException();
     }
 
